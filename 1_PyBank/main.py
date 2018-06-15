347576bffbd23692d64e50c21bd211ad6c387487
Brian_S <brian.salant37@gmail.com>
2018-06-14 22:28:34 -0400

    Here is the final edition of the Python Homework

---
+++ 1_PyBank/main.py
@@ -0,0 +1,73 @@
+### PyBank
+import os
+import csv  
+
+run = "y"
+while (run=="y"):
+    filerun = int(input("What number file would you like to run this on? "))
+    print("")
+## Read in the data
+    csvpath = os.path.join("bank_raw_data", "budget_data_%d.csv" %filerun)
+
+#Loop through the data and read it into lists for Months and Revenue
+    Months = []
+    Revenue = []
+    Revenue_change = []
+
+    with open(csvpath, newline="") as csvfile:
+        bank_data = csv.reader(csvfile, delimiter=",")
+        firstline = True
+        for row in bank_data:
+            if firstline:
+                firstline = False
+                continue
+            Months.append(row[0])
+            Revenue.append(row[1])
+        
+#Calculate the number of Months, turn revneue into integers
+    Num_rows = len(Months)
+    Revenue_nums = [int(rev) for rev in Revenue]
+    Tot_Revenue = sum(Revenue_nums)
+
+#Create the Revenue Change list
+    for i in range(1, Num_rows):
+        Revenue_change.append(Revenue_nums[i] - Revenue_nums[i-1])
+
+## Get the average
+    Average_change = sum(Revenue_change) / len(Revenue_change)
+
+##Get the min and the max of the revenues changes
+    Min_rc = min(Revenue_change)
+    Max_rc = max(Revenue_change)
+
+### revenue change is going to have 40 entries, from 0 to 39, each entry will belong to corresponding row + 1
+    Min_inx = int(Revenue_change.index(Min_rc)) + 1
+    Max_inx = int(Revenue_change.index(Max_rc)) + 1
+    
+#Match revenue changes to the correct month    
+    Min_month =  Months[Min_inx]
+    Max_month = Months[Max_inx]
+
+#print to terminal
+    print("Financial Analysis")
+    print("-" * 20)
+    print("Total Months: " + str(Num_rows))
+    print("Total Revenue: $" + '%.2f'%Tot_Revenue)
+    print("Average Revenue Change: $" + '%.2f'%Average_change)
+    print("Greatest Increase in Revenue: " + str(Max_month) + " ($" + '%.2f'%Max_rc + ")")
+    print("Greatest Decrease in Revenue: " + str(Min_month) + " ($" + '%.2f'%Min_rc + ")")
+    print("...")  
+
+## Print the results out to a text file
+    text_file = open("bank_output_%d.txt" %filerun, "w")
+    with open("bank_output.txt", "w") as text_file:
+        print("Financial Analysis", file = text_file)
+        print("-" * 20, file = text_file)
+        print("Total Months: " + str(Num_rows), file = text_file)
+        print("Total Revenue: $" + '%.2f'%Tot_Revenue, file = text_file)
+        print("Average Revenue Change: $" + '%.2f'%Average_change, file = text_file)
+        print("Greatest Increase in Revenue: " + str(Max_month) + " ($" + '%.2f'%Max_rc + ")", file = text_file)
+        print("Greatest Decrease in Revenue: " + str(Min_month) + " ($" + '%.2f'%Min_rc + ")", file = text_file)
+        print("...", file = text_file)
+    run = input("Would you like to run this on another file? (y|n) ")
+    print("")
---
+++ 2_PyPoll/main.py
@@ -0,0 +1,68 @@
+## PyPoll
+import os
+import csv
+
+run = "y"
+while (run=="y"):
+    filerun = int(input("What number file would you like to run this on? "))
+    print("")
+# Read in the data
+    csvpath = os.path.join("poll_raw_data", "election_data_%d.csv" %filerun)
+
+# The total number of votes cast, and create lists for candidates and their votes recweived
+    Total_votes = 0
+    Candidates = []
+    Candidate_votes = []
+
+    with open(csvpath, newline="") as csvfile:
+        poll_data = csv.reader(csvfile, delimiter=",")
+        firstline = True
+        for row in poll_data:
+            if firstline:
+                firstline = False
+                continue
+            Total_votes += 1
+            if row[2] not in Candidates:
+                Candidates.append(row[2])
+                Candidate_votes.append(1)
+            else:
+                inx = Candidates.index(row[2])
+                Candidate_votes[inx] += 1
+
+# Calculate the percentage of votes received by each candidate
+    Vote_percentage = []
+    Vote_percentage = [(can/Total_votes)*100 for can in Candidate_votes]
+
+# Find the winner
+    winning_index = Vote_percentage.index(max(Vote_percentage))
+    winner = Candidates[winning_index]
+
+# Print the Results to Terminal
+    print("...")
+    print("Election Results")
+    print("-" * 20)
+    print("Total Votes: " + str(Total_votes))
+    print("-" * 20)
+    Can_length = len(Candidates)
+    for x in range(0, Can_length):
+        print(f"{Candidates[x]}: {'%.1f'%Vote_percentage[x]}% ({Candidate_votes[x]})")
+    print("-" * 20)
+    print("Winner: " + str(winner))
+    print("...")  
+
+## Print the results out to a text file
+    text_file = open("poll_output_%d.txt" %filerun, "w")
+    with open("poll_output%d.txt" %filerun, "w") as text_file:
+        print("...", file = text_file)
+        print("Election Results", file = text_file)
+        print("-" * 20, file = text_file)
+        print("Total Votes: " + str(Total_votes), file = text_file)
+        print("-" * 20, file = text_file)
+        Can_length = len(Candidates)
+        for x in range(0, Can_length):
+            print(f"{Candidates[x]}: {'%.1f'%Vote_percentage[x]}% ({Candidate_votes[x]})", file = text_file)
+        print("-" * 20, file = text_file)
+        print("Winner: " + str(winner), file = text_file)
+        print("...", file = text_file)  
+    run = input("Would you like to run this on another file? (y|n) ")
+    print("")
